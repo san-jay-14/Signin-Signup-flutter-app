bad39b422d2c427737ab20e884f3a61daab73fdf
Sanjay <146640686+san-jay-14@users.noreply.github.com>
2024-02-04 00:58:48 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/sanjayflutterapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sanjayflutterapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/services.dart';
+import 'package:sanjayflutterapp/firebase_options.dart';
+import 'package:sanjayflutterapp/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.android,
+  );
+  runApp(MyApp());
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Email And Password Login',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
